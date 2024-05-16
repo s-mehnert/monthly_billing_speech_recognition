75dfed30cd3d08ca1882935c3d91d26cd9d86ed9
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2024-05-16 14:58:27 +0200

    setting up excel storage, importing audio from file

--- monthly_billing_speech_recognition__python.py
+++ monthly_billing_speech_recognition__python.py
@@ -1,5 +1,6 @@
 import speech_recognition as sr
 import time
+import openpyxl
 
 
 languages = ["en-EN", "de-DE"]
@@ -8,20 +9,26 @@ input_collection = list()
 rec = sr.Recognizer()
 mic = sr.Microphone()
 
+billing_audio = sr.AudioFile("test2.wav")
+with billing_audio as source:
+    audio = rec.record(source)
 
-def capture_input(prompt="Next"):
-    with mic as source:
-        # rec.adjust_for_ambient_noise(source)
-        print(prompt)
-        audio = rec.listen(source)
-        time.sleep(3)
-    return audio
+wb = openpyxl.load_workbook("monthly_billing.xlsx")
 
 
-def get_command(prompt="next, finished or print"):
-    print("Enter command")
-    command = create_text_from_audio(capture_input(prompt))
-    return command.lower()
+# def capture_input(prompt="Next"):
+#     with mic as source:
+#         # rec.adjust_for_ambient_noise(source)
+#         print(prompt)
+#         audio = rec.listen(source)
+#         time.sleep(3)
+#     return audio
+
+
+# def get_command(prompt="next, finished or print"):
+#     print("Enter command")
+#     command = create_text_from_audio(capture_input(prompt))
+#     return command.lower()
 
 
 def create_text_from_audio(audio):
@@ -36,26 +43,41 @@ def create_text_from_audio(audio):
         text = "Error"
     return text
 
-
-def create_entry():
-    entry = list()
-    prompts = ["Date", "Category", "Amount"]
-    print("Creating new entry: ")
-    for i in range(3):
-        time.sleep(3)
-        audio = capture_input(prompts[i])
-        text = create_text_from_audio(audio)
-        entry.append(text)
-    return entry
+billing_data = create_text_from_audio(audio)
+print(billing_data)
 
 
-command = get_command()
-while command not in ["next", "finished", "print"]:
-    command = get_command("Come again?")
-while command == "next":
-    new_entry = create_entry()
-    print("Adding to collection:", new_entry)
-    input_collection.append(new_entry)
-    command = get_command()
-if command == "finished":
-    print(input_collection)
+# def create_entry():
+#     entry = list()
+#     prompts = ["Date", "Category", "Amount"]
+#     print("Creating new entry: ")
+#     for i in range(3):
+#         time.sleep(3)
+#         audio = capture_input(prompts[i])
+#         text = create_text_from_audio(audio)
+#         entry.append(text)
+#     return entry
+
+
+current_month = ""
+ws = wb.create_sheet(current_month)
+
+# command = get_command()
+# while command not in ["next", "finished", "print"]:
+#     command = get_command("Come again?")
+# while command == "next":
+#     new_entry = create_entry()
+#     print("Adding to collection:", new_entry)
+#     input_collection.append(new_entry)
+#     command = get_command()
+# if command == "finished":
+#     print(input_collection)
+#     for entry in input_collection:
+#         row_number = ws.max_row + 1
+#         ws["A" + str(row_number)] = entry[0]
+#         ws["B" + str(row_number)] = entry[1]
+#         ws["C" + str(row_number)] = entry[2]
+#     wb.save("monthly_billing.xlsx")
+#     print("Data saved to file.")
+
+
