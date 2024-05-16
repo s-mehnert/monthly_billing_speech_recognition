6f2a461de68b68336188356dfeb70dcfdfb92031
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2024-05-16 17:26:20 +0200

    create logic to split up text and save to according excel cells

--- monthly_billing_speech_recognition__python.py
+++ monthly_billing_speech_recognition__python.py
@@ -6,33 +6,21 @@ import openpyxl
 languages = ["en-EN", "de-DE"]
 input_collection = list()
 
+current_year = "2024"
+
 rec = sr.Recognizer()
 mic = sr.Microphone()
 
-billing_audio = sr.AudioFile("test2.wav")
-with billing_audio as source:
-    audio = rec.record(source)
-
-wb = openpyxl.load_workbook("monthly_billing.xlsx")
+wb = openpyxl.load_workbook("billing_2.xlsx")
 
 
-# def capture_input(prompt="Next"):
-#     with mic as source:
-#         # rec.adjust_for_ambient_noise(source)
-#         print(prompt)
-#         audio = rec.listen(source)
-#         time.sleep(3)
-#     return audio
 
-
-# def get_command(prompt="next, finished or print"):
-#     print("Enter command")
-#     command = create_text_from_audio(capture_input(prompt))
-#     return command.lower()
-
-
-def create_text_from_audio(audio):
+def create_text_from_audio(audio_file):
     try:
+        billing_audio = sr.AudioFile(audio_file)
+        with billing_audio as source:
+            rec.adjust_for_ambient_noise(source)
+            audio = rec.record(source)
         text = rec.recognize_google(audio)
         print("Google Speech Recognition thinks you said " + text)
     except sr.UnknownValueError:
@@ -43,41 +31,65 @@ def create_text_from_audio(audio):
         text = "Error"
     return text
 
-billing_data = create_text_from_audio(audio)
+
+def sort_entries(entry_list):
+    check_again = list()
+    cleared = list()
+    for entry in entry_list:
+        if len(entry.split()) != 3:
+            check_again.append(entry)
+            print("Invalid entry:", entry)
+        else:
+            valid_entry = entry.split()
+            cleared.append(valid_entry)
+            print("Valid entry:", valid_entry)
+    return cleared, check_again
+
+
+def convert_month(m):  # add try except block
+    months = {"january": "01", "february": "02",  "march": "03", "april": "04", "may": "05",
+              "june": "06", "july": "07", "august": "08", "september": "09", "october": "10",
+              "november": "11", "december": "12"}
+    return months[m.lower()]
+
+
+def create_full_date(d, m, y):
+    return y + convert_month(m) + d
+
+
+billing_data = create_text_from_audio("test3.wav")
+bd_listed = billing_data.split("next")
 print(billing_data)
+print(bd_listed)
 
+valid_entries, invalid_entries = sort_entries(bd_listed)
+print("Valid entries:", valid_entries)
+print("Check again:", invalid_entries)
 
-# def create_entry():
-#     entry = list()
-#     prompts = ["Date", "Category", "Amount"]
-#     print("Creating new entry: ")
-#     for i in range(3):
-#         time.sleep(3)
-#         audio = capture_input(prompts[i])
-#         text = create_text_from_audio(audio)
-#         entry.append(text)
-#     return entry
-
-
-current_month = ""
+current_month = invalid_entries.pop(0).strip()
+print(current_month)
 ws = wb.create_sheet(current_month)
 
-# command = get_command()
-# while command not in ["next", "finished", "print"]:
-#     command = get_command("Come again?")
-# while command == "next":
-#     new_entry = create_entry()
-#     print("Adding to collection:", new_entry)
-#     input_collection.append(new_entry)
-#     command = get_command()
-# if command == "finished":
-#     print(input_collection)
-#     for entry in input_collection:
-#         row_number = ws.max_row + 1
-#         ws["A" + str(row_number)] = entry[0]
-#         ws["B" + str(row_number)] = entry[1]
-#         ws["C" + str(row_number)] = entry[2]
-#     wb.save("monthly_billing.xlsx")
-#     print("Data saved to file.")
+ws["A1"] = "Date"
+ws["B1"] = "Category"
+ws["C1"] = "Amount"
+
+for entry in valid_entries:
+    row_number = ws.max_row + 1
+    ws["A" + str(row_number)] = create_full_date(entry[0].strip(), current_month, current_year)
+    ws["B" + str(row_number)] = entry[1].strip()
+    ws["C" + str(row_number)] = entry[2].strip()
+
+for entry in invalid_entries:
+    row_number = ws.max_row + 1
+    ws["E" + str(row_number)] = entry
+
+wb.save("billing_2.xlsx")
+print("Data saved to file.")
+wb.close()
+    
+
+
+
 
 
