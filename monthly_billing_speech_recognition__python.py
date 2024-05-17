3641363953571b1a8ee3f15146ca0f7f4a35da7a
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2024-05-17 13:41:34 +0200

    fix various smaller issues related to formatting cells

--- monthly_billing_speech_recognition__python.py
+++ monthly_billing_speech_recognition__python.py
@@ -11,7 +11,7 @@ current_year = "2024"
 rec = sr.Recognizer()
 mic = sr.Microphone()
 
-wb = openpyxl.load_workbook("billing_2.xlsx")
+wb = openpyxl.load_workbook("billing.xlsx")
 
 
 
@@ -22,7 +22,7 @@ def create_text_from_audio(audio_file):
             rec.adjust_for_ambient_noise(source)
             audio = rec.record(source)
         text = rec.recognize_google(audio)
-        print("Google Speech Recognition thinks you said " + text)
+        print("Google Speech Recognition thinks you said\n" + text)
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
         text = "Unrecognized"
@@ -38,19 +38,33 @@ def sort_entries(entry_list):
     for entry in entry_list:
         if len(entry.split()) != 3:
             check_again.append(entry)
-            print("Invalid entry:", entry)
         else:
             valid_entry = entry.split()
             cleared.append(valid_entry)
-            print("Valid entry:", valid_entry)
     return cleared, check_again
 
 
-def convert_month(m):  # add try except block
+def convert_month(m):
     months = {"january": "01", "february": "02",  "march": "03", "april": "04", "may": "05",
               "june": "06", "july": "07", "august": "08", "september": "09", "october": "10",
               "november": "11", "december": "12"}
-    return months[m.lower()]
+    current_month = m
+    try:
+        current_month = months[m.lower()]
+    except:
+        print("Error: month not recognized.")
+    finally:
+        return current_month
+
+
+def convert_amount(a):
+    amount = a.strip()
+    try:
+        amount = float(a)
+    except:
+        print("Error: not a number")
+    finally:
+        return amount
 
 
 def create_full_date(d, m, y):
@@ -59,15 +73,15 @@ def create_full_date(d, m, y):
 
 billing_data = create_text_from_audio("test3.wav")
 bd_listed = billing_data.split("next")
-print(billing_data)
-print(bd_listed)
 
 valid_entries, invalid_entries = sort_entries(bd_listed)
-print("Valid entries:", valid_entries)
-print("Check again:", invalid_entries)
-
 current_month = invalid_entries.pop(0).strip()
-print(current_month)
+
+print("Creating spreadsheet for:", current_month)
+print("Valid entries:", valid_entries)
+print("All set" if not invalid_entries else "Check again:" + invalid_entries)
+
+
 ws = wb.create_sheet(current_month)
 
 ws["A1"] = "Date"
@@ -78,18 +92,18 @@ for entry in valid_entries:
     row_number = ws.max_row + 1
     ws["A" + str(row_number)] = create_full_date(entry[0].strip(), current_month, current_year)
     ws["B" + str(row_number)] = entry[1].strip()
-    ws["C" + str(row_number)] = entry[2].strip()
+    ws["C" + str(row_number)] = convert_amount(entry[2])
 
 for entry in invalid_entries:
     row_number = ws.max_row + 1
     ws["E" + str(row_number)] = entry
 
-wb.save("billing_2.xlsx")
+row_number = ws.max_row + 2
+ws["B" + str(row_number)] = "Total"
+ws["C" + str(row_number)] = "=SUM(C2:C" + str(row_number-1) + ")"
+
+wb.save("billing.xlsx")
 print("Data saved to file.")
 wb.close()
-    
-
-
-
 
 
